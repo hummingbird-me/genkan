6acf2391025fd24c5b38d5ff17f8650178447294
Peter Lejeck <peter.lejeck@gmail.com>
2018-06-14 14:44:05 -0700

    Add Framebuster concern

---
+++ app/controllers/concerns/framebuster.rb
@@ -0,0 +1,16 @@
+# Prevent clickjacking via headers.  This covers all our supported browsers: we do not need a
+# JavaScript framebuster.
+#
+# To disable this on a specific action, you can use `skip_before_action :framebuster, on: [:foo]`
+module Framebuster
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :framebuster
+  end
+
+  def framebuster
+    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers['Content-Security-Policy'] = "frame-ancestors 'none'"
+  end
+end
