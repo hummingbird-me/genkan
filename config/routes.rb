f6ea0bc479841aa2f897eddf9026adcb839c04a4
Emma Lejeck <nuck@kitsu.io>
2018-06-14 20:41:30 -0700

    Add routes for various grant types

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: 'grants' do
+    # Authorization Code Grant (three-legged)
+    scope constraints: ResponseTypeConstraint.new('code') do
+      get '/authorize', to: 'authorization_code#ask'
+      post '/authorize', to: 'authorization_code#grant'
+      delete '/authorize', to: 'authorization_code#reject'
+    end
+    scope constraints: GrantTypeConstraint.new('authorization_code') do
+      post '/token', to: 'authorization_code#token'
+    end
+
+    # Implicit Grant (two-legged)
+    scope constraints: ResponseTypeConstraint.new('token') do
+      get '/authorize', to: 'implicit#ask'
+      post '/authorize', to: 'implicit#grant'
+      delete '/authorize', to: 'implicit#reject'
+    end
+
+    # Resource Owner Password Grant
+    scope constraints: GrantTypeConstraint.new('password') do
+      post '/token', to: 'resource_owner_password#grant'
+    end
+
+    # Client Credentials Grant
+    scope constraints: GrantTypeConstraint.new('client_credentials') do
+      post '/token', to: 'client_credentials#grant'
+    end
+
+    # Assertion Grant (RFC 7521)
+    scope constraints: GrantTypeConstraint.new('assertion', /\Aurn:ietf:params:oauth:/) do
+      post '/token', to: 'assertion#grant'
+    end
+  end
 end
