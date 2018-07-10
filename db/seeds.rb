8da6e5519770307fbf684dc7a7048b07097b87df
Emma Lejeck <nuck@kitsu.io>
2018-07-09 19:03:15 -0700

    Generate Kitsu Application in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+kitsu = Client.create!(
+  name: 'Kitsu',
+  description: 'The Official Kitsu Website and Apps',
+  homepage: 'https://kitsu.io',
+  owner_id: 2, # Josh
+  permissions: 1
+)
+
+puts "Kitsu Application:"
+puts "    key: #{kitsu.key}"
+puts " secret: #{kitsu.secret}"
