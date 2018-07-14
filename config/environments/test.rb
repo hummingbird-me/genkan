b790fbf83ef6ab8a5beca3ede1fa6208463944cb
Emma Lejeck <nuck@kitsu.io>
2018-07-14 09:36:53 -0700

    Fix log level issue
    Apparently Rails docs are wrong

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,5 +41,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.log_level = Logger::WARN
+  config.log_level = :warn
 end
