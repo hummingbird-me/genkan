35219ef2394af1ce2b576060ee7f85937033d0e1
Emma Lejeck <nuck@kitsu.io>
2018-10-30 20:27:37 -0700

    Clean up Grant and Token models
    - Move things into WithExpiration and WithRevocation concerns

---
+++ app/models/concerns/with_expiration.rb
@@ -0,0 +1,28 @@
+# Manages records which expire using the `expires_at` column.
+module WithExpiration
+  extend ActiveSupport::Concern
+
+  included do
+    scope :expired, -> { where.not('expires_at < ?', Time.now) }
+
+    validates :expires_at, presence: true
+  end
+
+  # Set the expiration to occur by a duration
+  #
+  # @param duration [Integer] how far in the future to set the expiry
+  # @return [void]
+  def expires_in=(duration)
+    self.expires_at = duration.from_now
+  end
+
+  # @return [Integer] the time remaining before this expires
+  def expires_in
+    Time.now - expires_at
+  end
+
+  # @return [Boolean] whether this has expired yet
+  def expired?
+    expires_at&.past?
+  end
+end
--- app/models/token.rb
+++ app/models/token.rb
@@ -1,21 +1,11 @@
 class Token < ApplicationRecord
-  belongs_to :grant, required: true
+  include WithExpiration
+  include WithRevocation
 
-  validates :grant, presence: true
-  validates :expires_at, presence: true
+  belongs_to :client, required: true
 
-  def expires_in
-    expires_at - Time.now
-  end
-
-  # @return [Boolean] whether this grant has expired yet
-  def expired?
-    expires_at&.past?
-  end
-
-  def revoked?
-    revoked_at&.past? || grant.revoked?
-  end
+  validates :client, presence: true
+  validates :user_id, presence: true
 
   def acceptable?
     !(expired? || revoked?)
