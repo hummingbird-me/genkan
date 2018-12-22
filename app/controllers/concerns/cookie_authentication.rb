902f16245f6359bb24ba5773d8f83974993fbc98
Emma Lejeck <nuck@kitsu.io>
2018-12-21 21:48:50 -0800

    Add missing frozen_string_literals

--- app/controllers/concerns/cookie_authentication.rb
+++ app/controllers/concerns/cookie_authentication.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CookieAuthentication
   extend ActiveSupport::Concern
 
--- app/controllers/concerns/oauth2_redirect.rb
+++ app/controllers/concerns/oauth2_redirect.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module OAuth2Redirect
   extend ActiveSupport::Concern
 
