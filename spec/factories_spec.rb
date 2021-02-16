4d3056668b6039208d5150d6f17d35b0b7af0a9b
Emma Lejeck <nuck@kitsu.io>
2021-02-15 19:27:27 -0800

    Update factory linter spec

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe 'FactoryBot' do
-  before(:all) { DatabaseCleaner.start }
+  before(:all) do
+    DatabaseCleaner.allow_remote_database_url = true
+    DatabaseCleaner.start
+  end
   after(:all) { DatabaseCleaner.clean }
 
   FactoryBot.factories.each do |factory|
     it "#{factory.name} should pass lint" do
-      FactoryBot::Linter.new([factory], :factory).lint!
+      FactoryBot::Linter.new([factory]).lint!
     end
   end
 end
